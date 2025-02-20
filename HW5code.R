cd387f404f4c0f11c98ea4e40d3d4b60e2c431c1
Charlie Hooey <charleshooey@gmail.com>
2025-02-20 16:00:58 -0500

    Part 1 almost done

--- HW5code.R
+++ HW5code.R
@@ -1,4 +1,17 @@
 library(tidyverse)
 ######## Part 1 ########
+curr_tbl <- tibble(
+artist = character(),
+album = character(),
+song = character(),
+overall_loudness = numeric(),
+spectral_energy = numeric(),
+dissonance = numeric(),
+pitch_salience = numeric(),
+bpm = numeric(),
+beats_loudness = numeric(),
+danceability = numeric(),
+tuning_frequency = numeric()
+)
 current_filename <- "The Front Bottoms-Talon Of The Hawk-Au Revoir (Adios).json" 
-file_parts <- 
+json_file <- fromJSON(current_filename)
