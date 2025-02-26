d9b7ab5cc386f8f528a1c4c76810ec07462b9d50
Charlie Hooey <charleshooey@gmail.com>
2025-02-25 20:30:20 -0500

    Step 3 Done

--- HW5code.R
+++ HW5code.R
@@ -11,7 +11,7 @@ overall_loudness = pluck(json_data, "lowlevel", "loudness_ebu128", "integrated")
 spectral_energy = pluck(json_data, "lowlevel", "spectral_energy")
 dissonance = pluck(json_data, "lowlevel", "dissonance")
 pitch_salience = pluck(json_data, "lowlevel", "pitch_salience")
-bpm = pluck(json_data, "rhythm", "bpm"),
+bpm = pluck(json_data, "rhythm", "bpm")
 beats_loudness = pluck(json_data, "rhythm", "beats_loudness")
 danceability = pluck(json_data, "rhythm", "danceability")
 tuning_frequency = pluck(json_data, "tonal", "tuning_frequency")
@@ -58,3 +58,25 @@ essentia_df <- map_dfr(json_files, extract_essentia_data)
 ################################################################################
 # Step Three
 ################################################################################
+essentia_model_output <- read_csv("EssentiaOutput/EssentiaModelOutput.csv")
+
+# Compute averaged columns 
+essentia_model_output <- essentia_model_output |>
+  mutate(
+    valence = (deam_valence + emo_valence + muse_valence) / 3,
+    arousal = (emo_arousal + muse_arousal + deam_arousal) / 3,
+    aggressive = (nn_aggressive + eff_aggressive) / 2,
+    happy = (nn_happy + eff_happy) / 2,
+    party = (nn_party + eff_party) / 2,
+    relaxed = (nn_relax + eff_relax) / 2,
+    sad = (nn_sad + eff_sad) / 2,
+    acoustic = (nn_acoustic + eff_acoustic) / 2,
+    electric = (nn_electronic + eff_electronic) / 2,
+    instrumental = (nn_instrumental + eff_instrumental) / 2
+  )|>
+  rename(timbreBright = eff_timbre_bright) |>
+  select(artist, album, track, valence, arousal, aggressive, happy, 
+         party, relaxed, sad, acoustic, electric, instrumental, timbreBright)
+
+# View the cleaned data
+print(essentia_model_output)
